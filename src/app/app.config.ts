6f3cd8e4355221b3a0bf92c2b898280e24e8d9ed
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-21 01:27:03 -0300

    feat: invalid token interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth/auth.interceptor';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/http/error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([authInterceptor])
+      withInterceptors([
+        authInterceptor, 
+        errorInterceptor
+      ])
     )
   ]
 };
---
+++ src/app/core/http/error-interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
+import { catchError, throwError } from "rxjs";
+import { ApiException } from "../../../shared/models/api-exception";
+import { Router } from "@angular/router";
+import { inject } from "@angular/core";
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+const router = inject(Router);
+    return next(req).pipe(
+        catchError((error: HttpErrorResponse) => {
+            const apiError = error.error as ApiException;
+
+            switch (apiError?.errorCode) {
+                case "INVALID_TOKEN":
+                    router.navigate(['/login']);
+                    localStorage.removeItem("token");
+                    break;
+                default:
+                    break;
+            }
+            return throwError(() => apiError);
+        })
+    );
+};

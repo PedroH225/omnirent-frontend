a740721747b337236e7617e44e20f005b149ac27
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-23 18:37:38 -0300

    feat: authentication check methods

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { AuthModel, TokenResponse } from '../../features/auth/models/auth.model';
 import { environment } from '../../../environments/environment';
 import { Observable, tap } from 'rxjs';
+import { jwtDecode } from "jwt-decode";
+import { JwtPayload } from '../../features/auth/models/jwt.payload';
 
 @Injectable({
   providedIn: 'root'
@@ -14,19 +16,37 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(authModel: AuthModel): Observable<TokenResponse> {
-    localStorage.removeItem("token");
     return this.http.post<TokenResponse>(
       `${this.apiUrl}/auth/login`, authModel
     )
-    .pipe(
-      tap(response => localStorage.setItem("token", response.token))
-    );
+      .pipe(
+        tap(response => localStorage.setItem("token", response.token))
+      );
   }
 
   logout(): Observable<void> {
-  return this.http.patch<void>(`${this.apiUrl}/auth/logout`, null)
-    .pipe(
-      tap(() => localStorage.removeItem("token"))
-    );
-}
+    return this.http.patch<void>(`${this.apiUrl}/auth/logout`, null)
+      .pipe(
+        tap(() => localStorage.removeItem("token"))
+      );
+  }
+
+  getJwtPayload(token: string): JwtPayload {
+    return jwtDecode<JwtPayload>(token);
+  }
+
+  isAuthenticated(): boolean {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const payload = this.getJwtPayload(token);
+      return payload.exp * 1000 > Date.now();
+    } catch {
+      return false;
+    }
+  }
 }

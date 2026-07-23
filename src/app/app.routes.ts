4c342e15ec1d0b8e50bc6dcf214fc27a70866ae1
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-23 18:37:59 -0300

    feat: auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
+import { MyItemsComponent } from './features/items/pages/my-items/my-items.component/my-items.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'my-items',
+        canActivate: [authGuard],
+        component: MyItemsComponent
+    }
+];
+

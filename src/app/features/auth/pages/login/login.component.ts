d66cb7b2624bf83155b40a20426ea5ff2871396d
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-23 20:44:03 -0300

    feat: initial login page style

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { PasswordModule } from 'primeng/password'
+import { CardModule } from 'primeng/card'
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  standalone: true,
+  imports: [
+  PasswordModule,
+  CardModule,
+  ButtonModule,
+  InputTextModule,
+  FloatLabelModule,
+  FormsModule
+  ],
 })
 export class LoginComponent {
-
+  email: string = '';
+  password: string = '';
 }

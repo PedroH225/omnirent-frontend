936ea471599a7963332cc40330b143c6b929c77a
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-24 19:23:36 -0300

    feat: home page hero

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { CatalogBarComponent } from "@shared/components/catalog-bar/catalog-bar.component";
+import { GalleriaModule } from 'primeng/galleria';
 
 @Component({
   selector: 'app-home',
-  imports: [CatalogBarComponent],
+  imports: [CatalogBarComponent, GalleriaModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  images = [
+    {
+        itemImageSrc: 'assets/omni-hero-1.png',
+        thumbnailImageSrc: 'assets/omni-hero-1.png',
+        alt: 'Equipment rental'
+    },
+    {
+        itemImageSrc: 'assets/omni-hero-2.png',
+        thumbnailImageSrc: 'assets/omni-hero-2.png',
+        alt: 'Tools and equipment'
+    },
+    {
+        itemImageSrc: 'assets/omni-hero-3.png',
+        thumbnailImageSrc: 'assets/omni-hero-3.png',
+        alt: 'Audiovisual equipment'
+    }
+];
 }

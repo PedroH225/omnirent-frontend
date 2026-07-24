33937f0a70ec5a7a6fd40e268a07671c9bebe147
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-24 01:57:15 -0300

    feat: initial navbar style

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenuItem } from 'primeng/api';
+import { ImageModule } from 'primeng/image';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    RouterModule,
+    ImageModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+items: MenuItem[] = [];
 }

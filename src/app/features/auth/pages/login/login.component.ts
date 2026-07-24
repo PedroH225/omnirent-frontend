a74352f61d56a6fa0585e97361bbabbeef7af094
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-23 21:01:40 -0300

    feat: login

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -5,6 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { AuthService } from '../../../../core/auth/auth.service';
+import { AuthModel, TokenResponse } from '../../models/auth.model';
+import { Router } from '@angular/router';
+import { MyItemsComponent } from '../../../items/pages/my-items/my-items.component/my-items.component';
 
 @Component({
   selector: 'app-login',
@@ -12,15 +16,41 @@ import { FloatLabelModule } from 'primeng/floatlabel';
   styleUrl: './login.component.scss',
   standalone: true,
   imports: [
-  PasswordModule,
-  CardModule,
-  ButtonModule,
-  InputTextModule,
-  FloatLabelModule,
-  FormsModule
+    PasswordModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    FloatLabelModule,
+    FormsModule
   ],
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private router : Router) { }
+
+
+  login() {
+    localStorage.removeItem("token");
+
+    const payload = new AuthModel(
+      this.email,
+      this.password
+    );
+    this.authService.login(payload).subscribe(
+      (response) => {
+        const token: TokenResponse = response as TokenResponse;
+        localStorage.setItem("token", token.token);
+        this.router.navigate(['/my-items']);
+
+      }, (error) => {
+        console.log(error);
+      }
+    );
+
+
+  }
 }

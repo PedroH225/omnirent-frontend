ce7904141c4d2c6f9e7e7f1384efbfa409563767
PedroH225 <pedrodguimaraes20@gmail.com>
2026-08-13 22:02:55 -0300

    feat: user register

--- src/app/features/auth/components/save-user-form/save-user-form.component.ts
+++ src/app/features/auth/components/save-user-form/save-user-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Password } from "primeng/password";
 import { FloatLabel } from "primeng/floatlabel";
 import { Button } from "primeng/button";
@@ -8,12 +8,14 @@ import { UserFormModel } from '@features/auth/models/user-form-model';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
+import { FieldError } from '@shared/models/field-error';
+import { FieldErrorComponent } from "@shared/components/field-error/field-error.component";
 
 type UserFormMode = 'create' | 'edit';
 
 @Component({
   selector: 'app-save-user-form',
-  imports: [CommonModule, FormsModule, Password, FloatLabel, Button, Message, DatePicker, InputTextModule],
+  imports: [CommonModule, FormsModule, Password, FloatLabel, Button, DatePicker, InputTextModule, FieldErrorComponent],
   templateUrl: './save-user-form.component.html',
   styleUrl: './save-user-form.component.scss'
 })
@@ -21,13 +23,30 @@ export class SaveUserFormComponent {
   today: Date = new Date();
 
   @Input() mode: UserFormMode = 'create';
+  @Input() backendErrors: FieldError[] = [];
+  //@Input() user: user;
+
+  @Output() onSave = new EventEmitter<UserFormModel>()
+  @Output() formChange = new EventEmitter<UserFormModel>();
 
   form: UserFormModel = this.createEmptyForm();
 
-  errorMessage: string | undefined;
-
   save() {
-    throw new Error('Method not implemented.');
+    this.onSave.emit(this.form);
+  }
+
+  getFieldError(field: string): string | undefined {
+    return this.backendErrors.find(
+      error => error.field === field
+    )?.message;
+  }
+
+  onFieldChange(field: string): void {
+    this.backendErrors = this.backendErrors.filter(
+      error => error.field !== field
+    );
+
+    this.formChange.emit(this.form);
   }
 
   createEmptyForm(): UserFormModel {
--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 import { Card } from "primeng/card";
 import { DatePickerModule } from 'primeng/datepicker';
 import { SaveUserFormComponent } from "@features/auth/components/save-user-form/save-user-form.component";
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserFormModel } from '@features/auth/models/user-form-model';
+import { RegisterDto } from '@features/auth/models/register-model';
+import { FieldError } from '@shared/models/field-error';
+import { AuthService } from '@core/auth/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ApiValidationException } from '@shared/models/api-field-exception';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-register',
@@ -11,8 +18,53 @@ import { RouterLink } from '@angular/router';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-  register() {
-    throw new Error('Method not implemented.');
+  backendErrors: FieldError[] = [];
+
+  constructor(private authService: AuthService, private messageService: MessageService, private router: Router) { }
+
+  register(userForm: UserFormModel) {
+    const register: RegisterDto = this.toRegisterDto(userForm);
+
+    this.authService.registerUser(register).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Success',
+          detail: 'You were registered successfully.'
+        });
+        this.router.navigateByUrl("/auth/login");
+      },
+      error: (error: HttpErrorResponse) => {
+        const apiError = error.error as ApiValidationException;
+
+        if (apiError?.errorCode === 'VALIDATION_ERROR') {
+          this.backendErrors = apiError.fields;
+
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Validation failed',
+            detail: 'Please review the highlighted fields and try again.'
+          });
+
+          return;
+        }
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Unknown Error',
+          detail: 'Unknown error while trying to register. Try again later.'
+        });
+      }
+    })
   }
 
+  private toRegisterDto(userForm: UserFormModel): RegisterDto {
+    return {
+      name: userForm.name,
+      username: userForm.username,
+      email: userForm.email,
+      birthDate: userForm?.birthDate,
+      password: userForm.password,
+      repeatedPassword: userForm.repeatedPassword
+    };
+  }
 }

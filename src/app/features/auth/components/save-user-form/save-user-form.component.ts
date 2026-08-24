c57f9573ad5085236779ac561952e923b3fe48aa
PedroH225 <pedrodguimaraes20@gmail.com>
2026-08-24 16:08:37 -0300

    feat: local register user form validation

--- src/app/features/auth/components/save-user-form/save-user-form.component.ts
+++ src/app/features/auth/components/save-user-form/save-user-form.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Password } from "primeng/password";
 import { FloatLabel } from "primeng/floatlabel";
 import { Button } from "primeng/button";
-import { Message } from "primeng/message";
 import { DatePicker } from "primeng/datepicker";
 import { UserFormModel } from '@features/auth/models/user-form-model';
 import { CommonModule } from '@angular/common';
@@ -10,38 +9,63 @@ import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FieldError } from '@shared/models/field-error';
 import { FieldErrorComponent } from "@shared/components/field-error/field-error.component";
+import { SaveUserFormValidator } from '@features/auth/validators/user-form-validator';
 
 type UserFormMode = 'create' | 'edit';
 
 @Component({
   selector: 'app-save-user-form',
-  imports: [CommonModule, FormsModule, Password, FloatLabel, Button, DatePicker, InputTextModule, FieldErrorComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    Password,
+    FloatLabel,
+    Button,
+    DatePicker,
+    InputTextModule,
+    FieldErrorComponent
+  ],
   templateUrl: './save-user-form.component.html',
   styleUrl: './save-user-form.component.scss'
 })
 export class SaveUserFormComponent {
+
   today: Date = new Date();
 
   @Input() mode: UserFormMode = 'create';
   @Input() backendErrors: FieldError[] = [];
-  //@Input() user: user;
 
-  @Output() onSave = new EventEmitter<UserFormModel>()
+  @Output() onSave = new EventEmitter<UserFormModel>();
   @Output() formChange = new EventEmitter<UserFormModel>();
 
   form: UserFormModel = this.createEmptyForm();
 
-  save() {
+  localErrors: FieldError[] = [];
+
+  save(): void {
+    this.localErrors = SaveUserFormValidator.validate(this.form);
+
+    if (this.localErrors.length > 0) {
+      return;
+    }
+
     this.onSave.emit(this.form);
   }
 
   getFieldError(field: string): string | undefined {
-    return this.backendErrors.find(
+    return this.localErrors.find(
       error => error.field === field
-    )?.message;
+    )?.message
+      ?? this.backendErrors.find(
+        error => error.field === field
+      )?.message;
   }
 
   onFieldChange(field: string): void {
+    this.localErrors = this.localErrors.filter(
+      error => error.field !== field
+    );
+
     this.backendErrors = this.backendErrors.filter(
       error => error.field !== field
     );

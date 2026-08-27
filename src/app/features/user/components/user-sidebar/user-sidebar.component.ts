fd262088656ee5fd780d9fef6bcfe44a5b42be58
PedroH225 <pedrodguimaraes20@gmail.com>
2026-08-27 15:14:25 -0300

    feat: mobile user side bar

--- src/app/features/user/components/user-sidebar/user-sidebar.component.ts
+++ src/app/features/user/components/user-sidebar/user-sidebar.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { PanelMenuModule } from 'primeng/panelmenu';
+import { Button } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-user-sidebar',
-  imports: [PanelMenuModule],
+  imports: [PanelMenuModule, Button, DrawerModule],
   templateUrl: './user-sidebar.component.html',
-  styleUrl: './user-sidebar.component.scss'
+  styleUrl: './user-sidebar.component.scss',
 })
 export class UserSidebarComponent {
+  drawerVisible = false;
+
   readonly items: MenuItem[] = [
     {
       label: 'Dashboard',
       icon: 'pi pi-home',
-      routerLink: '/account'
+      routerLink: '/account',
     },
     {
       label: 'Listings',
@@ -22,14 +26,14 @@ export class UserSidebarComponent {
         {
           label: 'My Listings',
           icon: 'pi pi-list',
-          routerLink: '/account/my-items'
+          routerLink: '/account/my-items',
         },
         {
           label: 'Create Listing',
           icon: 'pi pi-plus',
-          routerLink: '/account/create-item'
-        }
-      ]
+          routerLink: '/account/create-item',
+        },
+      ],
     },
     {
       label: 'Rentals',
@@ -38,14 +42,14 @@ export class UserSidebarComponent {
         {
           label: 'Renting',
           icon: 'pi pi-shopping-cart',
-          routerLink: '/account/renting'
+          routerLink: '/account/renting',
         },
         {
           label: 'Renting Out',
           icon: 'pi pi-briefcase',
-          routerLink: '/account/renting-out'
-        }
-      ]
+          routerLink: '/account/renting-out',
+        },
+      ],
     },
     {
       label: 'Account',
@@ -54,29 +58,29 @@ export class UserSidebarComponent {
         {
           label: 'Profile',
           icon: 'pi pi-id-card',
-          routerLink: '/account/profile'
+          routerLink: '/account/profile',
         },
         {
           label: 'Addresses',
           icon: 'pi pi-map-marker',
-          routerLink: '/account/addresses'
+          routerLink: '/account/addresses',
         },
         {
           label: 'Security',
           icon: 'pi pi-lock',
-          routerLink: '/account/security'
+          routerLink: '/account/security',
         },
         {
           label: 'Settings',
           icon: 'pi pi-cog',
-          routerLink: '/account/settings'
-        }
-      ]
+          routerLink: '/account/settings',
+        },
+      ],
     },
     {
       label: 'Favorites',
       icon: 'pi pi-heart',
-      routerLink: '/account/favorites'
-    }
+      routerLink: '/account/favorites',
+    },
   ];
 }

204b83990c98358395ada2c3476b0ea9fdd2d68b
PedroH225 <pedrodguimaraes20@gmail.com>
2026-09-04 16:31:54 -0300

    refactor(auth): migrate JWT authentication to HttpOnly cookies

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,66 +1,40 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { AuthModel, TokenResponse } from '../../features/auth/models/auth.model';
+import { Injectable, signal } from '@angular/core';
+import { AuthModel } from '../../features/auth/models/auth.model';
 import { environment } from '../../../environments/environment';
-import { Observable, tap } from 'rxjs';
-import { jwtDecode } from "jwt-decode";
-import { JwtPayload } from '../../features/auth/models/jwt.payload';
+import { Observable, ReplaySubject, switchMap, tap } from 'rxjs';
+
 import { UserService } from '@core/user/user.service';
 import { RegisterDto } from '@features/auth/models/register-model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private readonly apiUrl: string = environment.apiUrl;
 
-  constructor(private http: HttpClient, private userService: UserService) { }
+  private authInitialized$ = new ReplaySubject<void>(1);
+
+  constructor(
+    private http: HttpClient,
+    private userService: UserService,
+  ) {}
 
   registerUser(register: RegisterDto): Observable<void> {
     return this.http.post<void>(`${this.apiUrl}/auth/register`, register);
   }
 
-  login(authModel: AuthModel): Observable<TokenResponse> {
-    return this.http.post<TokenResponse>(
-      `${this.apiUrl}/auth/login`, authModel
-    )
-      .pipe(
-        tap(response => {
-          localStorage.setItem("token", response.token);
-          this.userService.loadLoggedUserData();
-        }
-        )
-      );
+  login(authModel: AuthModel): Observable<void> {
+    return this.http
+      .post<void>(`${this.apiUrl}/auth/login`, authModel)
+      .pipe(switchMap(() => this.userService.loadLoggedUserData()));
   }
 
   logout(): Observable<void> {
-    return this.http.patch<void>(`${this.apiUrl}/auth/logout`, null)
-      .pipe(
-        tap(() => {
-          localStorage.removeItem("token");
-          this.userService.clearCurrentUser();
-        }
-        )
-      );
-  }
-
-  getJwtPayload(token: string): JwtPayload {
-    return jwtDecode<JwtPayload>(token);
-  }
-
-  isAuthenticated(): boolean {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const payload = this.getJwtPayload(token);
-      return payload.exp * 1000 > Date.now();
-    } catch {
-      return false;
-    }
+    return this.http.patch<void>(`${this.apiUrl}/auth/logout`, null).pipe(
+      tap(() => {
+        this.userService.clearCurrentUser();
+      }),
+    );
   }
 }

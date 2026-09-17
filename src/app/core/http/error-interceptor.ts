c3b039a682d966296ea6c95d83b28b5c2875db56
PedroH225 <pedrodguimaraes20@gmail.com>
2026-09-16 22:05:03 -0300

    feat: redirect on forbidden

--- src/app/core/http/error-interceptor.ts
+++ src/app/core/http/error-interceptor.ts
@@ -1,15 +1,25 @@
-import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
-import { catchError, throwError } from "rxjs";
-import { ApiException } from "../../shared/models/api-exception";
-import { Router } from "@angular/router";
-import { inject } from "@angular/core";
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { ApiException } from '../../shared/models/api-exception';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-const router = inject(Router);
-    return next(req).pipe(
-        catchError((error: HttpErrorResponse) => {
-            const apiError = error.error as ApiException;
-            return throwError(() => error);
-        })
-    );
+  const router = inject(Router);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      const apiError = error.error as ApiException;
+      switch (apiError.errorCode) {
+        case 'FORBIDDEN':
+          if (router.url.startsWith('/admin')) {
+            router.navigate(['/']);
+          }
+          break;
+
+        default:
+          break;
+      }
+      return throwError(() => error);
+    }),
+  );
 };

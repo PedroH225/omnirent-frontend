2488578cfdb0547f6e3dd6bacead6dc421f61925
PedroH225 <pedrodguimaraes20@gmail.com>
2026-09-18 15:40:46 -0300

    feat: save user locale

--- src/app/shared/components/locale-selector/locale-selector.component.ts
+++ src/app/shared/components/locale-selector/locale-selector.component.ts
@@ -1,6 +1,10 @@
 import { Component, Signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthStateService } from '@core/auth/auth-state.service';
 import { Locale, LocaleService } from '@core/i18n/locale.service';
+import { UserPreferences } from '@core/user/model/user-preferences-model';
+import { UserPreferencesService } from '@core/user/user-preferences.service';
+import { UserService } from '@core/user/user.service';
 import { Select } from 'primeng/select';
 
 @Component({
@@ -25,11 +29,32 @@ export class LocaleSelectorComponent {
     },
   ];
 
-  constructor(private localeService: LocaleService) {
+  constructor(
+    private localeService: LocaleService,
+    private preferencesService: UserPreferencesService,
+    private authState: AuthStateService,
+  ) {
     this.locale = localeService.locale;
   }
 
   setLocale(locale: Locale): void {
     this.localeService.setLocale(locale);
   }
+
+  onLocaleChange(locale: Locale): void {
+  this.localeService.setLocale(locale);
+
+  if (this.authState.isAuthenticated()) {
+    const body: UserPreferences = {
+      locale,
+      timezone: null,
+    };
+
+    this.preferencesService.savePreferences(body).subscribe({
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+}
 }

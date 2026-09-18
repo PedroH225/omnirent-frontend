7b48a4755c6eff9c4ad4130e9810c6a6eb401af6
PedroH225 <pedrodguimaraes20@gmail.com>
2026-09-18 15:24:26 -0300

    feat: save language locally

--- src/app/core/i18n/locale.service.ts
+++ src/app/core/i18n/locale.service.ts
@@ -3,14 +3,10 @@ import { Injectable, signal } from '@angular/core';
 export type Locale = 'en-US' | 'pt-BR';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocaleService {
-
-  private readonly SUPPORTED_LOCALES = new Set<Locale>([
-    'en-US',
-    'pt-BR'
-  ]);
+  private readonly SUPPORTED_LOCALES = new Set<Locale>(['en-US', 'pt-BR']);
 
   private readonly _locale = signal<Locale>(this.getSystemLocale());
 
@@ -18,17 +14,32 @@ export class LocaleService {
 
   setLocale(locale: string): void {
     if (this.SUPPORTED_LOCALES.has(locale as Locale)) {
-      this._locale.set(locale as Locale);
+      const resolvedLocale = locale as Locale;
+
+      this._locale.set(resolvedLocale);
+      this.saveLocaleLocally(resolvedLocale);
     }
   }
 
   private getSystemLocale(): Locale {
-    const language = navigator.language.split('-')[0].toLowerCase();
+    const localLocale = localStorage.getItem('locale') ?? navigator.language;
 
-    if (language === 'pt') {
+    const resolvedLocale = this.resolveLocale(localLocale);
+
+    this.saveLocaleLocally(resolvedLocale);
+
+    return resolvedLocale;
+  }
+
+  private resolveLocale(language: string): Locale {
+    if (language.toLowerCase().startsWith('pt')) {
       return 'pt-BR';
     }
 
     return 'en-US';
+  }
+
+  private saveLocaleLocally(locale: Locale): void {
+    localStorage.setItem('locale', locale);
   }
 }

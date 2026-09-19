f4bcfdf5abc2b27a8b7a3f1a6856cd53070e7cd1
PedroH225 <pedrodguimaraes20@gmail.com>
2026-09-19 01:13:25 -0300

    feat: cached rental status

--- src/app/core/rental/rental.service.ts
+++ src/app/core/rental/rental.service.ts
@@ -2,35 +2,41 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PageResponse } from '../../shared/models/page.response.model';
 import { RentalDisplayModel } from '@features/rentals/model/rental-display-model';
-import { Observable, shareReplay } from 'rxjs';
+import { Observable, of, shareReplay, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { RentalDetailModel } from '@features/rentals/model/rental-detail-model';
 import { RentalEnumsResponse } from '@features/rentals/model/rental-enums-model';
 import { CreateRentalRequest } from '@features/rentals/model/create-rental-request-model';
 import { RentalCreatedModel } from '@features/rentals/model/rental-created-model';
 import { RentalOperationalModel } from '@features/rentals/model/rental-operational-model ';
+import { CacheDuration, CacheService } from '@core/cache/cache.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RentalService {
-
   private readonly apiUrl: string = environment.apiUrl;
 
-  private rentalEnums$?: Observable<RentalEnumsResponse>;
+  private readonly RENTAL_ENUMS_CACHE_KEY = 'rental-enums';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cacheService: CacheService) {}
 
   createRental(request: CreateRentalRequest): Observable<RentalCreatedModel> {
     return this.http.post<RentalDetailModel>(`${this.apiUrl}/rental`, request);
   }
 
   startPreparing(rentalId: string): Observable<void> {
-    return this.http.patch<void>(`${this.apiUrl}/rental/${rentalId}/start-preparing`, {});
+    return this.http.patch<void>(
+      `${this.apiUrl}/rental/${rentalId}/start-preparing`,
+      {},
+    );
   }
 
   cancelRental(rentalId: string): Observable<void> {
-    return this.http.patch<void>(`${this.apiUrl}/rental/${rentalId}/cancel`, {});
+    return this.http.patch<void>(
+      `${this.apiUrl}/rental/${rentalId}/cancel`,
+      {},
+    );
   }
 
   shipRental(rentalId: string): Observable<void> {
@@ -38,54 +44,89 @@ export class RentalService {
   }
 
   markInUse(rentalId: string): Observable<RentalDisplayModel> {
-    return this.http.patch<RentalDisplayModel>(`${this.apiUrl}/rental/${rentalId}/in-use`, {});
+    return this.http.patch<RentalDisplayModel>(
+      `${this.apiUrl}/rental/${rentalId}/in-use`,
+      {},
+    );
   }
 
   requestReturn(rentalId: string): Observable<void> {
-    return this.http.patch<void>(`${this.apiUrl}/rental/${rentalId}/request-return`, {});
+    return this.http.patch<void>(
+      `${this.apiUrl}/rental/${rentalId}/request-return`,
+      {},
+    );
   }
 
   shipReturnRental(rentalId: string): Observable<void> {
-    return this.http.patch<void>(`${this.apiUrl}/rental/${rentalId}/return-shipped`, {});
+    return this.http.patch<void>(
+      `${this.apiUrl}/rental/${rentalId}/return-shipped`,
+      {},
+    );
   }
 
   confirmReturn(rentalId: string): Observable<void> {
-    return this.http.patch<void>(`${this.apiUrl}/rental/${rentalId}/returned`, {});
+    return this.http.patch<void>(
+      `${this.apiUrl}/rental/${rentalId}/returned`,
+      {},
+    );
   }
 
   getRentalDetail(rentalId: string): Observable<RentalDetailModel> {
-    return this.http.get<RentalDetailModel>(`${this.apiUrl}/rental/find/${rentalId}`);
+    return this.http.get<RentalDetailModel>(
+      `${this.apiUrl}/rental/find/${rentalId}`,
+    );
   }
 
   getOperationalData(rentalId: string): Observable<RentalOperationalModel> {
-    return this.http.get<RentalOperationalModel>(`${this.apiUrl}/rental/find/operational/${rentalId}`);
+    return this.http.get<RentalOperationalModel>(
+      `${this.apiUrl}/rental/find/operational/${rentalId}`,
+    );
   }
 
-  getRenting(page: number, size: number): Observable<PageResponse<RentalDisplayModel>> {
-    const params = new HttpParams()
-      .set("page", page)
-      .set("size", size);
+  getRenting(
+    page: number,
+    size: number,
+  ): Observable<PageResponse<RentalDisplayModel>> {
+    const params = new HttpParams().set('page', page).set('size', size);
 
-    return this.http.get<PageResponse<RentalDisplayModel>>(this.apiUrl + "/rental/find/rented", { params });
+    return this.http.get<PageResponse<RentalDisplayModel>>(
+      this.apiUrl + '/rental/find/rented',
+      { params },
+    );
   }
 
-  getRentingOut(page: number, size: number): Observable<PageResponse<RentalDisplayModel>> {
-    const params = new HttpParams()
-      .set("page", page)
-      .set("size", size);
+  getRentingOut(
+    page: number,
+    size: number,
+  ): Observable<PageResponse<RentalDisplayModel>> {
+    const params = new HttpParams().set('page', page).set('size', size);
 
-    return this.http.get<PageResponse<RentalDisplayModel>>(this.apiUrl + "/rental/find/userRentals", { params });
+    return this.http.get<PageResponse<RentalDisplayModel>>(
+      this.apiUrl + '/rental/find/userRentals',
+      { params },
+    );
   }
 
   getRentalEnums(): Observable<RentalEnumsResponse> {
-    if (!this.rentalEnums$) {
-      this.rentalEnums$ = this.http
-        .get<RentalEnumsResponse>(`${this.apiUrl}/rental/enums`)
-        .pipe(
-          shareReplay(1)
-        );
+    const cached = this.cacheService.get<RentalEnumsResponse>(
+      this.RENTAL_ENUMS_CACHE_KEY,
+    );
+
+    if (cached) {
+      return of(cached);
     }
 
-    return this.rentalEnums$;
+    return this.http
+      .get<RentalEnumsResponse>(`${this.apiUrl}/rental/enums`)
+      .pipe(
+        tap((response) => {
+          this.cacheService.set(
+            this.RENTAL_ENUMS_CACHE_KEY,
+            response,
+            CacheDuration.LONG,
+          );
+        }),
+        shareReplay(1),
+      );
   }
 }
